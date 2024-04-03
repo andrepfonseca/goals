297278b883bc7437b200b1be31f9059295191391
rethink-daniel <daniel.vidal@rethink.dev>
2024-04-03 16:04:01 -0300

    feat: adding AddProductCard page

--- src/screens/AddProductCard/index.tsx
+++ src/screens/AddProductCard/index.tsx
@@ -1,45 +1,108 @@
-import React, { useState } from "react";
-import { SafeAreaView, View, Button, TextInput } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  SafeAreaView,
+  View,
+  Button,
+  TextInput,
+  Image,
+  Touchable,
+  TouchableOpacity,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 import { styles } from "./styles";
-import { ProductCardType, Typography } from "components";
+import * as ImagePicker from "expo-image-picker";
 
-export const AddProductCard = ({ route }: any) => {
-  const [item, setItem] = React.useState<ProductCardType>();
-  const [priceInput, setPriceInput] = useState("");
+import { Typography } from "components";
+import { createItem } from "database";
+
+export const AddProductCard = () => {
+  const [priceInput, setPriceInput] = useState<number>(0);
   const [titleInput, setTitleInput] = useState("");
-  const [imageInput, setImageInput] = useState("");
+  const [imageInput, setImageInput] = useState<
+    Array<ImagePicker.ImagePickerAsset>
+  >([]);
 
-  const handleImageSelection = () => {
-    // Implement logic to access image from gallery
+  const handleImageSelection = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 4],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImageInput(result.assets);
+      console.log(result.assets);
+    }
   };
 
-  const handleFormSubmit = () => {
-    const product = {
-      image: imageInput,
-      price: priceInput,
-      title: titleInput,
-    };
+  const handleFormSubmit = async () => {
+    const response = await createItem(
+      titleInput,
+      imageInput[0]?.uri,
+      priceInput,
+      priceInput
+    );
 
+    console.log(response);
     // Handle form submission logic
   };
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.content}>
-        <Typography variant="pageTitle">{"Add Product"}</Typography>
-        <Button title="Select Image" onPress={handleImageSelection} />
-        <TextInput
-          placeholder="Price"
-          value={priceInput}
-          onChangeText={setPriceInput}
-        />
-        <TextInput
-          placeholder="Title"
-          value={titleInput}
-          onChangeText={setTitleInput}
-        />
-        <Button title="Submit" onPress={handleFormSubmit} />
-      </View>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 50}
+        style={{
+          flex: 1,
+        }}
+      >
+        <ScrollView style={styles.content}>
+          <TouchableOpacity onPress={handleImageSelection}>
+            {imageInput[0]?.uri ? (
+              <Image
+                source={{
+                  uri: imageInput[0]?.uri,
+                }}
+                style={styles.image}
+              />
+            ) : (
+              <Image
+                source={require("../../assets/defaultImage.png")}
+                style={styles.image}
+              />
+            )}
+          </TouchableOpacity>
+          <View style={styles.formContainer}>
+            <Typography variant="pageTitle">
+              {"Adicione o nome e valor do produto:"}
+            </Typography>
+            <TextInput
+              placeholder="Preço Total"
+              value={priceInput?.toString()}
+              onChangeText={(text: string) => setPriceInput(Number(text))}
+            />
+            <TextInput
+              placeholder="Title"
+              value={titleInput}
+              onChangeText={setTitleInput}
+            />
+            <TouchableOpacity style={styles.button} onPress={handleFormSubmit}>
+              <Typography
+                variant="body"
+                style={{
+                  color: "#FFFFFF",
+                  fontWeight: "500",
+                }}
+              >
+                {"SALVAR"}
+              </Typography>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
--- src/screens/AddProductCard/styles.ts
+++ src/screens/AddProductCard/styles.ts
@@ -12,5 +12,27 @@ export const styles = StyleSheet.create({
     flex: 1,
     display: "flex",
     width: "100%",
+    gap: 20,
+  },
+  image: {
+    flexGrow: 1,
+    width: "100%",
+    height: 400,
+  },
+  formContainer: {
+    flex: 1,
+    display: "flex",
+    width: "100%",
+    padding: 20,
+    gap: 20,
+  },
+  button: {
+    width: "100%",
+    backgroundColor: "#2DB6CF",
+    height: 45,
+    borderRadius: 25,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
